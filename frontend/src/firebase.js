cd8bbc93c1bce33bbf16c95daa353c05f8deee1c
Harshwardhan199 <harshwardhan199saini@gmail.com>
2025-08-13 18:57:21 +0530

    Removed sensitive data(api keys, etc)

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6Vro97551toXDOZJJBI5xH5iOWUwCk1U",
-  authDomain: "chatbot-ai-7470d.firebaseapp.com",
-  projectId: "chatbot-ai-7470d",
-  storageBucket: "chatbot-ai-7470d.firebasestorage.app",
-  messagingSenderId: "473031483278",
-  appId: "1:473031483278:web:dba45275990890c0d6cedf"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
