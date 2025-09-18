1aa6f55d6fba7d388c424b5efa027e27a58d1176
Harshwardhan199 <harshwardhan199saini@gmail.com>
2025-09-18 10:58:08 +0530

    removed proxy(dev feature)

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 
 import { authStore } from "../context/AuthContext";
+import { config } from "../config";
 
 const api = axios.create({
-  baseURL: `/api`,
+  baseURL: `${config.BACKEND_URL}/api`,
   withCredentials: true,
 });
 
 const getValidAccessToken = async () => {
   let token = authStore.getAccessToken();
   //console.log("Token ->: ", token);
-
+  
   if (!token) {
     //console.log("Token ->->: ", token);
     // Token missing → call refresh
     try {
       const res = await axios.post(
-        `/api/auth/refresh`,
+        `${config.BACKEND_URL}/api/auth/refresh`,
         {},
         { withCredentials: true }
       );
@@ -56,7 +57,7 @@ api.interceptors.response.use(
       console.log("Retring for new access token /refresh");
 
       try {
-        const res = await axios.post(`/api/auth/refresh`, {}, { withCredentials: true });
+        const res = await axios.post( `${config.BACKEND_URL}/api/auth/refresh`, {}, { withCredentials: true } );
 
         const newAccessToken = res.data.accessToken;
         authStore.updateAccessToken(newAccessToken);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-import { config } from "./src/config";
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  server: {
-    proxy: {
-      '/api': {
-        target: config.BACKEND_URL,
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
-
 })
 
