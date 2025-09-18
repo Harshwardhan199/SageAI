0a9cc3a4e58a45c9eade9f691313a1f3bb09f227
Harshwardhan199 <harshwardhan199saini@gmail.com>
2025-09-18 09:38:43 +0530

    token refresh logic updated

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -187,7 +187,7 @@ const refresh = async (req, res) => {
 
         console.log("Refresh token: ", refreshToken);
         
-        if (!refreshToken) return res.json({ message: "No refresh token" });
+        if (!refreshToken) return res.status(201).json({ message: "No refresh token" });
 
         jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, async (err, decoded) => {
             if (err) return res.status(403).json({ message: "Invalid refresh token" });
